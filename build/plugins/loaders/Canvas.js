6a8014a58666749066433b20c88c555e961bafb5
庙上攥 <81863241+MiaoShangZuan@users.noreply.github.com>
2026-06-29 17:36:03 +0800

    Delete build directory

--- build/plugins/loaders/Canvas.js
+++
@@ -1,4 +0,0 @@
-const CANVAS_SYSTEM = document.createElement('canvas');
-const DRAW_SYSTEM = CANVAS_SYSTEM.getContext( '2d', { willReadFrequently: true } );
-
-export { CANVAS_SYSTEM, DRAW_SYSTEM };
